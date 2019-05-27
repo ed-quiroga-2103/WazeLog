af33369d4e0c0f51321965484de6da44362af28c
Racso08 <oscar080899@gmail.com>
2019-05-27 16:15:38 -0600

    Regla para paradas intermedias

--- Procesamiento_del_Lenguaje.pl
+++ Procesamiento_del_Lenguaje.pl
@@ -15,3 +15,26 @@ validacion_aux2(T):- ( last_element(T,X),lugar([X]) -> write('no se que pasa')
 
 obtener_ubicacion(T,L):- ( oracion(T),last_element(T,X),lugar([X]) -> last_element(T,L)
                          ; validacion_aux(T) ).
+
+intermedios(T,Y):- ( T = [si]-> intermedios_aux2(Y)
+                   ; T = [no]-> write(Y)
+                   ; intermedio_aux(T,Y) ).
+
+intermedios_aux2(Y):- write('¿Cual seria?'),nl,read_line_to_codes(user_input,Cs), atom_codes(A, Cs), atomic_list_concat(L, ' ', A), intermedio_aux(L,Y).
+
+intermedio_aux(T,Y):- ( oracion(T), last_element(T,X), sustantivo([X]) -> intermedio_aux3(X,Y)
+                      ; validacion_aux(T) ).
+
+intermedio_aux3(T,Y):- write('¿Cual ' + T ),nl,read_line_to_codes(user_input,Cs), atom_codes(A, Cs), atomic_list_concat(L, ' ', A), intermedio_aux4(L,Y).
+
+intermedio_aux4(T,Y):- ( list_butlast(T,S),reverse(S,[],L),concatenar([cartago],L,P),reverse(P,[],E), oracion(E) -> intermedio_aux5(T,Y)
+                       ; validacion_aux(T) ).
+
+intermedio_aux5(T,Y):- last_element(T,X), write('Donde se encuentra '+ X +'?'),nl,read_line_to_codes(user_input,Cs), atom_codes(A, Cs),atomic_list_concat(L, ' ', A), intermedio_aux6(L,Y).
+
+
+
+intermedio_aux6(T,Y):- (oracion(T), last_element(T,X), lugar([X]) -> intermedio_aux7(T,Y)
+                      ; validacion_aux(T) ).
+
+intermedio_aux7(T,Y):- last_element(T,X), lugar([X]),concatenar([X],Y,Z), write('¿Algun destino intermedio?'),nl,read_line_to_codes(user_input,Cs), atom_codes(A, Cs), atomic_list_concat(L, ' ', A), intermedios(L,Z).
